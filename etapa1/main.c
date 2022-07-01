ce3af892c553ddd32383cee08dca1a359a7e594d
Tiago Magnus <tiago.magnus@avidity.se>
2022-06-30 23:20:24 -0300

    Corrigindo parse de strings

--- etapa1/main.c
+++ etapa1/main.c
@@ -16,8 +16,21 @@ int main(int argc, char** argv)
 {
   FILE *gold = 0;
   int token = 0;
+  int rd = 0;
   int lineNr = 0;
   int i=1;
+
+  if (0==(yyin = fopen(argv[1],"r")))
+  {
+    printf("Cannot open file %s... \n",argv[1]);
+    exit(1);
+  }
+
+  if (0==(gold = fopen(argv[2],"r")))
+  {
+    printf("Cannot open file %s... \n",argv[2]);
+    exit(1);
+  }
 
   while (isRunning())
   {
@@ -25,39 +38,41 @@ int main(int argc, char** argv)
       break;
 
     token = yylex();
-		if(lineNr != getLineNumber()){
-			lineNr = getLineNumber();
-			printLineNumber();
-		}
+		// if(lineNr != getLineNumber()){
+		// 	lineNr = getLineNumber();
+		// 	printLineNumber();
+		// }
+    fscanf(gold,"%d",&rd);
 
-    switch(token){
-      case KW_CHAR: 		printf("KW_CHAR "); break;
-			case KW_INT: 		printf("KW_INT "); break;
-			case KW_FLOAT: 		printf("KW_FLOAT "); break;
+    switch(rd){
+      case KW_CHAR: 		  printf("KW_CHAR "); break;
+			case KW_INT: 		    printf("KW_INT "); break;
+			case KW_FLOAT: 		  printf("KW_FLOAT "); break;
 
-      case KW_IF:      printf("KW_IF "); break;
-      case KW_ELSE:      printf("KW_ELSE "); break;
+      case KW_IF:         printf("KW_IF "); break;
+      case KW_ELSE:       printf("KW_ELSE "); break;
       case KW_WHILE:      printf("KW_WHILE "); break;
-      case KW_READ:      printf("KW_READ "); break;
+      case KW_READ:       printf("KW_READ "); break;
       case KW_PRINT:      printf("KW_PRINT "); break;
-      case KW_RETURN:      printf("KW_RETURN "); break;
+      case KW_RETURN:     printf("KW_RETURN "); break;
 
-      case ASSIGNMENT:     printf("ASSIGNMENT "); break;
-      case OPERATOR_LE:     printf("OPERATOR_LE "); break;
-      case OPERATOR_GE:     printf("OPERATOR_GE "); break;
-      case OPERATOR_EQ:     printf("OPERATOR_EQ "); break;
-      case OPERATOR_DIF:     printf("OPERATOR_DIF "); break;
+      case ASSIGNMENT:    printf("ASSIGNMENT "); break;
+      case OPERATOR_LE:   printf("OPERATOR_LE "); break;
+      case OPERATOR_GE:   printf("OPERATOR_GE "); break;
+      case OPERATOR_EQ:   printf("OPERATOR_EQ "); break;
+      case OPERATOR_DIF:  printf("OPERATOR_DIF "); break;
 
-      case TK_IDENTIFIER:       printf("TK_IDENTIFIER "); break;
-      case LIT_INTEGER:       printf("LIT_INTEGER "); break;
-      case LIT_FLOAT:       printf("LIT_FLOAT "); break;
-      case LIT_CHAR:       printf("LIT_CHAR "); break;
-      case LIT_STRING:       printf("LIT_STRING "); break;
-			case TOKEN_ERROR:	printf("TOKEN_ERROR "); break;
+      case TK_IDENTIFIER: printf("TK_IDENTIFIER "); break;
+      case LIT_INTEGER:   printf("LIT_INTEGER "); break;
+      case LIT_FLOAT:     printf("LIT_FLOAT "); break;
+      case LIT_CHAR:      printf("LIT_CHAR "); break;
+      case LIT_STRING:    printf("LIT_STRING "); break;
+			case TOKEN_ERROR:	  printf("TOKEN_ERROR "); break;
 
-			default: 			printf("%s ", yytext); break;
+			default: 			      printf("%s ", yytext); break;
 		}
   }
 
   hashPrint();
+  printLineNumber();
 }
