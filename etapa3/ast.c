0ad9d0c3d10683a1dac8a0734bed11f13981b078
PedroHoerlle <63522614+PedroHoerlle@users.noreply.github.com>
2022-08-02 05:18:58 -0300

    AST implementada.

--- etapa3/ast.c
+++ etapa3/ast.c
@@ -4,3 +4,62 @@ Tiago de Carvalho Magnus - 00287710
 Pedro Hoerlle de Oliveira - 00288548
 */
 
+#include "ast.h"
+
+AST *astCreate (int type, HASH_NODE *symbol, AST *son0, AST *son1, AST *son2, AST *son3)
+{
+    AST* newnode;
+    newnode = (AST*) calloc(1, sizeof(AST));
+    newnode->type = type;
+    newnode->symbol = symbol;
+    newnode->son[0] = son0;
+    newnode->son[1] = son1;
+    newnode->son[2] = son2;
+    newnode->son[3] = son3;
+    return newnode;
+}
+
+void astPrint (AST *node, int level)
+{
+    int i = 0;
+    if (node == 0)
+    {
+        return;
+    }
+    for (i = 0; i < level; ++i)
+    {
+        fprintf (stderr, "  ");
+    }
+    fprintf (stderr, "ast(");
+    switch (node->type)
+    {
+        case AST_SYMBOL: fprintf (stderr, "AST_SYMBOL"); break;
+        case AST_ADD: fprintf (stderr, "AST_ADD"); break;
+        case AST_SUB: fprintf (stderr, "AST_SUB"); break;
+        case AST_DIV: fprintf (stderr, "AST_DIV"); break;
+        case AST_MUL: fprintf (stderr, "AST_MUL"); break;
+        case AST_LT: fprintf (stderr, "AST_LT"); break;
+        case AST_GT: fprintf (stderr, "AST_GT"); break;
+        case AST_LE: fprintf (stderr, "AST_LE"); break;
+        case AST_GE: fprintf (stderr, "AST_GE"); break;
+        case AST_EQ: fprintf (stderr, "AST_EQ"); break;
+        case AST_DIF: fprintf (stderr, "AST_DIF"); break;
+        case AST_AND: fprintf (stderr, "AST_AND"); break;
+        case AST_OR: fprintf (stderr, "AST_OR"); break;
+        //case AST: fprintf (stderr, "AST"); break;
+        default: fprintf (stderr, "AST_UNKNOWN"); break;
+    }
+    if (node->symbol != 0)
+    {
+        fprintf (stderr, ", %s\n", node->symbol->text);
+    }
+    else
+    {
+        fprintf (stderr, ", 0\n");
+    }
+    for (i = 0; i < MAX_SONS; ++i)
+    {
+        astPrint (node->son[i], level + 1);
+    }
+}
+
--- etapa3/ast.h
+++ etapa3/ast.h
@@ -4,3 +4,37 @@ Tiago de Carvalho Magnus - 00287710
 Pedro Hoerlle de Oliveira - 00288548
 */
 
+#ifndef AST_HEADER
+#define AST_HEADER
+
+#include "hash.h"
+
+#define MAX_SONS 4
+
+#define AST_SYMBOL 1
+#define AST_ADD 2
+#define AST_SUB 3
+#define AST_DIV 4
+#define AST_MUL 5
+#define AST_LT 6
+#define AST_GT 7
+#define AST_LE 8
+#define AST_GE 9
+#define AST_EQ 10
+#define AST_DIF 11
+#define AST_AND 12
+#define AST_OR 13
+//#define AST 
+
+
+typedef struct astnode
+{
+    int type;
+    HASH_NODE *symbol;
+    struct astnode *son[MAX_SONS];
+} AST;
+
+AST *astCreate (int type, HASH_NODE *symbol, AST *son0, AST *son1, AST *son2, AST *son3);
+void astPrint (AST *node, int level);
+
+#endif
